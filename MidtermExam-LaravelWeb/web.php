28f853bc716234ce4e3b37258eb3d4239bcc11df
benedictochia <benedictochia24@gmail.com>
2025-10-29 09:04:49 +0800

    Delete MidtermExam-LaravelWeb directory

--- MidtermExam-LaravelWeb/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
-
-Route::get('/', [PagesController::class, 'home']);
-Route::get('/about', [PagesController::class, 'about']);
-Route::get('/gallery', [PagesController::class, 'gallery']);
